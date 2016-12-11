f802e4b1046deb257dd54ef01c67ffab11f5d340
Eunice Lin <eunicel@mit.edu>
2016-12-11 18:56:33 -0500

    added cdf code and graph

--- collab_pilot/parse_json_data.py
+++ collab_pilot/parse_json_data.py
@@ -1,9 +1,13 @@
+import os
+import numpy
+import matplotlib.pyplot as mlp
 import json
 import pprint
-from decimal import Decimal
+from matplotlib.pyplot import *
 
 pp = pprint.PrettyPrinter(indent=2)
 
+best_scores_each_user = []
 with open('tacit-collab-export.json') as data:
 	data = json.load(data)
 	for pilotnum in data:
@@ -18,6 +22,18 @@ with open('tacit-collab-export.json') as data:
 						meta = user[problem]["meta"]
 						for structure_id in structures:
 							structure = structures[structure_id]
-							if (Decimal(structure["weight"])/100) < best_score:
-								best_score = Decimal(structure["weight"])/100
-						print best_score
+							if not "weight" in structure:
+								# ran out of time event accidentally saved
+								# in structures instead of events, fixed
+								continue
+							elif (float(structure["weight"])/100) < best_score:
+								best_score = float(structure["weight"])/100
+						best_scores_each_user.append(best_score)
+print best_scores_each_user
+n_bins = len(best_scores_each_user)
+n, bins, patches = mlp.hist(best_scores_each_user, n_bins, normed=True, histtype="step", cumulative=True)
+mlp.ylim(0,1)
+mlp.ylabel("Percentile")
+mlp.xlabel("Score [$]")
+mlp.title("Pilot Study: Individual User Best Score CDF")
+mlp.savefig("graphs/best_scores_each_user_CDF.png")
