8a00a55e96dcc364b6b442ee62b8f1aae8286c8c
Eunice Lin <eunicel@mit.edu>
2016-12-12 14:57:15 -0500

    added code and graph for plotting score vs num times load from teammate

--- collab_pilot/parse_json_data.py
+++ collab_pilot/parse_json_data.py
@@ -8,6 +8,7 @@ from matplotlib.pyplot import *
 pp = pprint.PrettyPrinter(indent=2)
 
 best_scores_each_user = []
+score_vs_load_count = []
 with open('tacit-collab-export.json') as data:
 	data = json.load(data)
 	for pilotnum in data:
@@ -20,6 +21,15 @@ with open('tacit-collab-export.json') as data:
 						events = user[problem]["events"]
 						structures = user[problem]["structures"]
 						meta = user[problem]["meta"]
+
+						# count number of times users loaded from teammate
+						user_load_count = 0
+						for event_id in events:
+							event = events[event_id]
+							if event["type"] == "load from teammate" or event["type"] == "load from teammate current":
+								user_load_count += 1
+
+						# find best score for user
 						for structure_id in structures:
 							structure = structures[structure_id]
 							if not "weight" in structure:
@@ -29,11 +39,25 @@ with open('tacit-collab-export.json') as data:
 							elif (float(structure["weight"])/100) < best_score:
 								best_score = float(structure["weight"])/100
 						best_scores_each_user.append(best_score)
-print best_scores_each_user
-n_bins = len(best_scores_each_user)
-n, bins, patches = mlp.hist(best_scores_each_user, n_bins, normed=True, histtype="step", cumulative=True)
-mlp.ylim(0,1)
-mlp.ylabel("Percentile")
-mlp.xlabel("Score [$]")
-mlp.title("Pilot Study: Individual User Best Score CDF")
-mlp.savefig("graphs/best_scores_each_user_CDF.png")
+						score_vs_load_count.append((best_score, user_load_count))
+
+# Plot individual best score CDF
+#print best_scores_each_user
+#n_bins = len(best_scores_each_user)
+#n, bins, patches = mlp.hist(best_scores_each_user, n_bins, normed=True, histtype="step", cumulative=True)
+#mlp.ylim(0,1)
+#mlp.ylabel("Percentile")
+#mlp.xlabel("Score [$]")
+#mlp.title("Pilot Study: Individual User Best Score CDF")
+#mlp.savefig("graphs/best_scores_each_user_CDF.png")
+
+# Plot score vs. number of times user loaded from teammate
+scores = [s for (s, l) in score_vs_load_count]
+loads = [l for (s, l) in score_vs_load_count]
+mlp.scatter(loads, scores)
+mlp.xlabel("Number of Times User Loaded Teammate's Structure")
+mlp.ylabel("Score [$]")
+mlp.title("Pilot Study: Score vs. Number of Times User Loaded From Teammate")
+mlp.savefig("graphs/score_vs_num_times_load")
+
+
